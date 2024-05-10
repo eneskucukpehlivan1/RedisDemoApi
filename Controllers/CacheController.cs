5c971f22e9b1c41ccb3a69ad05e68ca653e248dd
eneskucukpehlivan1 <eneskucukpehlivan1@gmail.com>
2024-05-10 23:22:25 +0300

    Redis için örnek bir entegrasyon yapıldı.

---
+++ Controllers/CacheController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisDemoApi.Interfaces;
+using RedisDemoApi.Models;
+
+namespace RedisDemoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CacheController : ControllerBase
+    {
+        private readonly IRedisCacheService _redisCacheService;
+
+        public CacheController(IRedisCacheService redisCacheService)
+        {
+            _redisCacheService = redisCacheService;
+        }
+        [HttpPost("{key}")]
+        public async Task<IActionResult> Get(string key)
+        {
+            return Ok(await _redisCacheService.GetValueAsync(key));
+        }
+
+        [HttpPost("set")]
+        public async Task<IActionResult> Set([FromBody] RedisCacheRequestModel redisCacheRequestModel)
+        {
+            await _redisCacheService.SetValueAsync(redisCacheRequestModel.Key, redisCacheRequestModel.Value);
+            return Ok();
+        }
+
+        [HttpPost("{key}")]
+        public async Task<IActionResult> Delete(string key)
+        {
+            await _redisCacheService.Clear(key);
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult DeleteAll()
+        {
+            _redisCacheService.ClearAll();
+            return Ok();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using RedisDemoApi.Interfaces;
+using RedisDemoApi.Services;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,13 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-var redisConnection = ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var redisConnection = ConnectionMultiplexer.Connect($"{builder.Configuration.GetConnectionString("Redis")},allowAdmin=true");
 builder.Services.AddSingleton<IConnectionMultiplexer>(redisConnection);
+builder.Services.AddSingleton<IRedisCacheService, RedisCacheService>();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
